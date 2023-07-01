7bc8e61f6c6b9d01f26552745971a3cf59fed277
wellitonaraujo <wellitonaraujodev@hotmail.com>
2023-07-01 08:02:20 -0300

    craete folder structure

---
+++ MessengerSwiftUI/App/MessengerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerSwiftUIApp.swift
+//  MessengerSwiftUI
+//
+//  Created by Welliton da Conceicao de Araujo on 01/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MessengerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
